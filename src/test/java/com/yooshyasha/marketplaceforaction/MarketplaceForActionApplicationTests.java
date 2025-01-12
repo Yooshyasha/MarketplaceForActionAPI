75134132ce1c0a8ea98ef592bd973df8339299d1
Yooshyasha <mejzikrejzi66@gmail.com>
2025-01-12 13:54:18 +0500

    Initial Commit

---
+++ src/test/java/com/yooshyasha/marketplaceforaction/MarketplaceForActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yooshyasha.marketplaceforaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceForActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
